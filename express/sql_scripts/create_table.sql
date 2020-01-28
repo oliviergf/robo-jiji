869d1ef91c4a1ec9902a92955c899fd6feaf8f6b
oliviergf <olivier.gauthier.farley@hotmail.com>
2020-01-27 22:19:31 -0500

    finished sql shemas; inserts 5 tables

--- express/sql_scripts/create_table.sql
+++ express/sql_scripts/create_table.sql
@@ -1,28 +1,8 @@
-DROP TABLE IF EXISTS Apparts;
 DROP TABLE IF EXISTS Apparts_Users;
-DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS Zones;
+DROP TABLE IF EXISTS Apparts;
 DROP TABLE IF EXISTS Geo;
-
-
-CREATE TABLE IF NOT EXISTS `devbot`.`Apparts` (
-  `_id` int NOT NULL AUTO_INCREMENT ,
-  `title` VARCHAR(300) ,
-  `price` SMALLINT ,
-  FOREIGN KEY (geo_id) NOT NULL REFERENCES Geo (_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  `link` VARCHAR(255) ,
-  `description` VARCHAR(400) ,
-  `date` DATETIME ,
-  `rooms` ENUM( '1', '2', '3', '4', '5', '6', '7' ) ,
-  PRIMARY KEY (`_id`) );
-
-CREATE TABLE IF NOT EXISTS `devbot`.`Apparts_Users` (
-  `_id` int NOT NULL AUTO_INCREMENT ,
-  FOREIGN KEY (appart_id) REFERENCES Apparts (_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (users_id) REFERENCES Users (_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  PRIMARY KEY (appart_id, users_id,)
-  PRIMARY KEY (`_id`) );
-
+DROP TABLE IF EXISTS Zones;
+DROP TABLE IF EXISTS Users;
 
 CREATE TABLE IF NOT EXISTS `devbot`.`Users` (
   `_id` int NOT NULL AUTO_INCREMENT ,
@@ -34,12 +14,37 @@ CREATE TABLE IF NOT EXISTS `devbot`.`Users` (
 
 CREATE TABLE IF NOT EXISTS `devbot`.`Zones` (
   `_id` int NOT NULL AUTO_INCREMENT ,
+  `users_id` int NOT NULL,
   FOREIGN KEY (users_id) REFERENCES Users (_id) ON DELETE RESTRICT ON UPDATE CASCADE,
   PRIMARY KEY (`_id`) );
 
+
 CREATE TABLE IF NOT EXISTS `devbot`.`Geo` (
   `_id` int NOT NULL AUTO_INCREMENT ,
-  FOREIGN KEY (zone_id) REFERENCES Zones (_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  `zone_id` int NOT NULL ,
   `lat` DECIMAL(10, 8) ,
   `lng` DECIMAL(11, 8)  ,
+  FOREIGN KEY (zone_id) REFERENCES Zones (_id) ON DELETE RESTRICT ON UPDATE CASCADE,
   PRIMARY KEY (`_id`) );
+
+CREATE TABLE IF NOT EXISTS `devbot`.`Apparts` (
+  `_id` int NOT NULL AUTO_INCREMENT ,
+  `geo_id` int NOT NULL  ,
+  `title` VARCHAR(300) ,
+  `price` SMALLINT ,
+  `link` VARCHAR(255) ,
+  `description` VARCHAR(400) ,
+  `date` DATETIME ,
+  `rooms` ENUM( '1', '2', '3', '4', '5', '6', '7' ) ,
+  FOREIGN KEY (geo_id) REFERENCES Geo (_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (`_id`) );
+
+CREATE TABLE IF NOT EXISTS `devbot`.`Apparts_Users` (
+  `appart_id` int NOT NULL ,
+  `users_id` int NOT NULL ,
+  FOREIGN KEY (appart_id) REFERENCES Apparts (_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (users_id) REFERENCES Users (_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (appart_id,users_id)
+  );
+
+
