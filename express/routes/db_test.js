b106a7d729682bfaec596c227bf9c630a07abb3d
oliviergf <olivier.gauthier.farley@hotmail.com>
2020-02-01 17:58:34 -0500

    tested add user into DB, OK

--- express/routes/db_test.js
+++ express/routes/db_test.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var router = express.Router();
+const Models = require("../models");
 
 /* GET React home page. */
-router.get("/", function(req, res, next) {
+router.get("/", async function(req, res, next) {
+  const jane = await Models.Users.create({
+    firstname: "Jane2",
+    lastname: "teste2r",
+    password: "itsnotsa2fe",
+    email: "email@com.net"
+  });
+
   res.send("db tests");
 });
 
