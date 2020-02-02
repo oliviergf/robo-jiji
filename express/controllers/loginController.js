d2fab0c7bd1a2552a352e930d0a7d9e53d135b52
oliviergf <olivier.gauthier.farley@hotmail.com>
2020-02-01 21:04:12 -0500

    creates login controller

---
+++ express/controllers/loginController.js
@@ -0,0 +1,21 @@
+const model = require("../models");
+
+LoginController = {
+  login: async userinfo => {
+    //check that no user has the same email provided
+    let userCount = await model.Users.count({
+      where: {
+        email: userinfo.email,
+        password: userinfo.password
+      }
+    });
+
+    if (userCount === 1) {
+      console.log("user exist create session and send back JWT?");
+    } else {
+      console.log("user doesnt exist");
+    }
+  }
+};
+
+module.exports = LoginController;
--- express/routes/login.js
+++ express/routes/login.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var passport = require("passport");
+const loginController = require("../controllers/loginController");
 
 /* GET React home page. */
 router.get("/", function(req, res, next) {
@@ -12,12 +13,14 @@ router.get("/", function(req, res, next) {
 });
 
 /* POST React home page. */
-router.post("/", function(req, res, next) {
-  const email = req.body.email;
-  const password = req.body.password;
-  res.send(
-    `POST this dude is logged the fuck in! email:${email} pass:${password}`
-  );
+router.post("/", async function(req, res, next) {
+  let userInfo = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  await loginController.login(userInfo);
+  res.send("login");
 });
 
 module.exports = router;
