3453192d9f7de37cc1824da4ce61b5f3645b1908
oliviergf <olivier.gauthier.farley@hotmail.com>
2020-02-11 13:47:02 -0500

    adds functionnal nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,12 @@ import "typeface-roboto";
 import "./App.css";
 import Button from "@material-ui/core/Button";
 
-//to here
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+
+import { Switch, Route, Link } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -33,28 +32,54 @@ function App() {
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
+            <Button edge="start" color="inherit">
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/register"
+              >
+                register
+              </Link>
+            </Button>
+            <Button edge="start" color="inherit">
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/testSession"
+              >
+                testSesh
+              </Link>
+            </Button>
             <Typography variant="h6" className={classes.title}>
-              News
+              Kijiji Bot App
             </Typography>
-            <Button color="inherit">Login</Button>
+            <Button edge="start" color="inherit">
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/login"
+              >
+                login
+              </Link>
+            </Button>
           </Toolbar>
         </AppBar>
       </div>
-      bonjour hi!
-      <Register />
-      <Login />
-      <TestSession />
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+      <div className="body">
+        <Switch>
+          {/* If the current URL is /about, this route is rendered
+            while the rest are ignored */}
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/testSession">
+            <TestSession />
+          </Route>
+          <Route path="/">
+            <div>This is our home</div>
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
