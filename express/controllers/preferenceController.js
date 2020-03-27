7e86e88924f68b6ab8a96cc2ce537f2f9a191b39
oliviergf <olivier.gauthier.farley@hotmail.com>
2020-03-27 02:36:22 -0400

    saves preferences to user

--- express/controllers/preferenceController.js
+++ express/controllers/preferenceController.js
@@ -2,24 +2,29 @@ const model = require("../models");
 
 preferenceController = {
   savePreferences: async (userId, prefs) => {
-    console.log(prefs);
-    console.log(userId);
-    //todo: check that no user has the same email provided
-    //todo: handle all exceptions
-    // let user = await model.Users.findAll({
-    //   where: {
-    //     email: userinfo.email,
-    //     password: userinfo.password
-    //   }
-    // });
+    let user = await model.Users.findOne({
+      where: {
+        _id: userId
+      }
+    });
 
-    // //todo: implements last_login update :)
-    // //todo: we may want to send more info than that.
-    // if (user.length === 1) {
-    //   return user[0].dataValues;
-    // } else {
-    //   return null;
-    // }
+    if (user) {
+      console.log(user);
+      user.dateAvailable = prefs.dateAvailable;
+      user.priceStart = prefs.priceStart;
+      user.priceEnd = prefs.priceEnd;
+      user.rooms = JSON.stringify(prefs.rooms);
+      user.numberBedrooms = JSON.stringify(prefs.numberBedrooms);
+      user.parkingAvailable = prefs.parkingAvailable;
+      user.wheelchairAccessible = prefs.wheelchairAccessible;
+      user.petsAllowed = prefs.petsAllowed;
+      user.furnished = prefs.furnished;
+    }
+
+    user.save().catch(error => {
+      console.log(error);
+      //todo handle error
+    });
   }
 };
 
