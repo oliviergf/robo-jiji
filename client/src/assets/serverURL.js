414d38f43e038edbf5fa6c368d52fb91f201aed1
oliviergf <olivier.gauthier.farley@hotmail.com>
2020-04-14 22:56:39 -0400

    adds try cath in zone route

--- client/src/assets/serverURL.js
+++ client/src/assets/serverURL.js
@@ -2,7 +2,7 @@ const url = "https://jijibot-express.herokuapp.com";
 
 export default url;
 
-//heroku url: https://jijibot-express.herokuapp.com/
+//heroku url: https://jijibot-express.herokuapp.com
 
 // dev url: https://localhost:3000
 // prod: ""
--- express/controllers/zoneController.js
+++ express/controllers/zoneController.js
@@ -1,52 +1,55 @@
 const model = require("../models");
 
 zoneController = {
-  createZone: async zoneInfo => {
+  createZone: async (zoneInfo) => {
     //google maps doesnt add the closing point on the polygon but mysql needs it
-    const firstPoint = zoneInfo.path[0];
-    zoneInfo.path.push(firstPoint);
+    try {
+      const firstPoint = zoneInfo.path[0];
+      zoneInfo.path.push(firstPoint);
 
-    await model.Zones.create({
-      zoneId: zoneInfo.zoneId,
-      UserId: zoneInfo.userId,
-      polygon: {
-        type: "Polygon",
-        coordinates: [zoneInfo.path]
-      },
-      name: zoneInfo.name
-    });
-
+      await model.Zones.create({
+        zoneId: zoneInfo.zoneId,
+        UserId: zoneInfo.userId,
+        polygon: {
+          type: "Polygon",
+          coordinates: [zoneInfo.path],
+        },
+        name: zoneInfo.name,
+      });
+    } catch (error) {
+      log.o("could not save zone", error);
+    }
     //todo: checker quels apparts relativement recents sont dans la nouvelle zone?
   },
-  getAllZones: async userId => {
+  getAllZones: async (userId) => {
     //query db
     const result = await model.Zones.findAll({
       where: {
-        UserId: userId
-      }
+        UserId: userId,
+      },
     });
 
     //formats result
     let userZones = [];
-    result.map(zone => {
+    result.map((zone) => {
       // removes the last element to be google formatted polygon in UI
       zone.dataValues.polygon.coordinates[0].splice(-1, 1);
       userZones.push({
         zoneId: zone.dataValues.zoneId,
         path: zone.dataValues.polygon.coordinates[0],
-        name: zone.dataValues.name
+        name: zone.dataValues.name,
       });
     });
     return userZones;
   },
-  deleteZone: async zoneInfo => {
+  deleteZone: async (zoneInfo) => {
     //Removes element from DB
     await model.Zones.destroy({
       where: {
-        zoneId: zoneInfo.zoneId
-      }
+        zoneId: zoneInfo.zoneId,
+      },
     });
-  }
+  },
 };
 
 module.exports = zoneController;
