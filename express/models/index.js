b6f5f23bf7b3969f4ef3a6971c2fe92645ec5c36
oliviergf <olivier.gauthier.farley@hotmail.com>
2020-04-16 16:46:20 -0400

    unfucks index models

--- express/models/index.js
+++ express/models/index.js
@@ -16,6 +16,16 @@ Sequelize.GEOMETRY.prototype._stringify = function _stringify(value, options) {
     wkx.Geometry.parseGeoJSON(value).toWkt()
   )})`;
 };
+Sequelize.GEOMETRY.prototype._bindParam = function _bindParam(value, options) {
+  return `ST_GeomFromText(${options.bindParam(
+    wkx.Geometry.parseGeoJSON(value).toWkt()
+  )})`;
+};
+Sequelize.GEOGRAPHY.prototype._stringify = function _stringify(value, options) {
+  return `ST_GeomFromText(${options.escape(
+    wkx.Geometry.parseGeoJSON(value).toWkt()
+  )})`;
+};
 Sequelize.GEOGRAPHY.prototype._bindParam = function _bindParam(value, options) {
   return `ST_GeomFromText(${options.bindParam(
     wkx.Geometry.parseGeoJSON(value).toWkt()
